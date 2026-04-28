50d3a457885440431d4678a45d058982858b90f7
Christopher Luvumbo <christopherluvumbo.dev@gmail.com>
2026-04-28 22:05:53 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,11 @@ import { IntroSection } from "@/components/AProposDeNous"
 import { FeatureList } from "@/components/FeatureList"
 import { ContentSection } from "@/components/ContentSection"
 import { Services } from "@/components/Services"
-import { ScrollToTop } from "@/components/ScrollToTop"
 import Footer from "@/components/footer"
 
 export default function Home() {
   return (
     <>
-      <ScrollToTop />
       <HeroSection />
       <FeatureList/>
       <Services />
